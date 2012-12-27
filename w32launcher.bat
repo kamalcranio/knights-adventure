affeacb6e1a3b41f27419c503dfd24a8ed6c9654
kamalcranio <kamalcranio@gmail.com>
2012-12-27 21:59:31 -0200

    Update w32launcher.bat
    Inserted license terms

--- w32launcher.bat
+++ w32launcher.bat
@@ -1,3 +1,8 @@
+REM A permissão é garantida, livre de custo, para qualquer pessoa que obtenha uma cópia deste software e os documentos associados ao mesmo (o "Software"), com o poder de usar, modificar, retirar partes do código para futuros softwares, publicar, distribuir, sublicenciar, e/ou vender cópias do Software, e permitir à indivíduos por quem o software está sendo fornecido para tal.
+
+REM Os termos acima e os comunicados a seguir deverão se encontrar em qualquer cópia ou parte substancial do Software.
+
+REM O SOFTWARE É FORNECIDO SEM GARANTIA DE QUALQUER FORMA, EXPLÍCITA OU IMPLÍCITA, LIMITADO APENAS ÀS GARANTIAS DE MERCADO, RE-FORMAÇÃO PARA ALGUM PROPÓSITO EM PARTICULAR E NÃO INFRIGIMENTO. EM NENHUM MOMENTO OS AUTORES OU DONOS DOS DIREITOS DO SOFTWARE DEVEM SER PUNIDOS POR QUALQUER CULPA, DANOS OU OUTRAS RAZÕES DECORRENTES DO SOFTWARE DESCRITO.
 @echo off
 set versao = 0.0.3aWin32
 if exist "%versao%.*" (goto:existe)
