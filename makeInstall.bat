11aa807c1636f06c1dab2a3ced338af8401f7d4a
kamalcranio <kamalcranio@gmail.com>
2012-12-27 22:08:57 -0200

    Update makeInstall.bat

--- makeInstall.bat
+++ makeInstall.bat
@@ -1,3 +1,17 @@
+REM knights-adventure windows easy-way compiler
+
+REM A permissão é garantida, livre de custo, para qualquer pessoa que obtenha uma cópia deste software
+REM e os documentos associados ao mesmo (o "Software"), com o poder de usar, modificar, retirar partes do código
+REM para futuros softwares, publicar, distribuir, sublicenciar, e/ou vender cópias do Software, e permitir à
+REM indivíduos por quem o software está sendo fornecido para tal.
+
+REM Os termos acima e os comunicados a seguir deverão se encontrar em qualquer cópia ou parte substancial do Software.
+
+REM O SOFTWARE É FORNECIDO SEM GARANTIA DE QUALQUER FORMA, EXPLÍCITA OU IMPLÍCITA, LIMITADO APENAS ÀS GARANTIAS
+REM DE MERCADO, RE-FORMAÇÃO PARA ALGUM PROPÓSITO EM PARTICULAR E NÃO INFRIGIMENTO. EM NENHUM MOMENTO OS AUTORES
+REM OU DONOS DOS DIREITOS DO SOFTWARE DEVEM SER PUNIDOS POR QUALQUER CULPA, DANOS OU OUTRAS RAZÕES DECORRENTES
+REM DO SOFTWARE DESCRITO.
+
 @echo off
 set titulo=KNIGHTS ADVENTURE 0.0.3a - INSTALADOR VER 0.5a - By Kamalcranio
 title %titulo%
